f5552bf0407d6b7fca7c7fb83a3874d63b7458e1
soumya <soumyajeevanigi@gmail.com>
2020-01-28 22:01:22 +0530

    added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppComponent } from './app.component';
 
+import { GitServiceService } from 'src/services/git-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { OrderByPipe } from './order-by.pipe';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderByPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    DataTablesModule
   ],
-  providers: [],
+  providers: [GitServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
